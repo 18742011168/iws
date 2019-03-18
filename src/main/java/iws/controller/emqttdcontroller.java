7b65f87d7f012a16d2d6670e4c8d7feeef5d3294
王佳华 <1113920978@qq.com>
2019-03-18 14:53:38 +0800

    3-18加入mqtt发送端，修改springboot运行端口号为9090

---
+++ src/main/java/iws/controller/emqttdcontroller.java
@@ -0,0 +1,21 @@
+package iws.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import iws.mqtt.MqttGateway;
+
+@RestController
+
+public class emqttdcontroller {
+
+	@Autowired
+    private MqttGateway mqttGateway;
+	 @RequestMapping("/emqttd")
+	    public String sendMqtt(){
+	        mqttGateway.sendToMqtt("123","hello");
+	        return "OK";
+	    }
+	
+}
